dac641b01bb8d78b2fe4f1212f5307b998f7a710
Eduardo Sánchez <eduardosr1599@gmail.com>
2022-07-21 12:54:07 -0400

    Add a button to let user choose grid dimensions

--- grid.js
+++ grid.js
@@ -1,19 +1,41 @@
 const grid=document.querySelector(".grid");
 const gridDimension=grid.getBoundingClientRect();
 const gridWidth=gridDimension.width;
-let base=16
-let dimension=base**2;
+let base=8
 
-for (let i=1;i<=dimension;i++){
+function createGrid(base,container){
+    for (let i=1;i<=base**2;i++){
     const div=document.createElement("div");
     div.setAttribute("class","square");
     let squareDimension=gridWidth/base+"px";
     div.style.width=squareDimension;
     div.style.height=squareDimension;
-    grid.appendChild(div);
+    container.appendChild(div);
+    }
 }
 
-const squares=document.querySelectorAll(".square")
-squares.forEach(square=>{
+createGrid(base,grid);
+fillSquare();
+
+function fillSquare(){
+    const squares=document.querySelectorAll(".square")
+    squares.forEach(square=>{
     square.addEventListener("mouseover",()=>square.style.backgroundColor="yellow")
+    })
+}
+
+const btn=document.querySelector("button");
+btn.addEventListener("click",()=>{
+    let newBase=+prompt("Number of squares per row",16);
+    if (newBase>100 || newBase===0){
+        alert ("Please input a number up to 100.");
+        return false;
+    }
+    let previousGrid=document.querySelector("div");
+    document.body.removeChild(previousGrid);
+    let newGrid=document.createElement("div");
+    newGrid.setAttribute("class","grid");
+    document.body.appendChild(newGrid);
+    createGrid(newBase,newGrid);
+    fillSquare();
 })
